4f0b72781899fb18b6c14bab9d71312d326e6eed
Zak Goedegebuur <zakgoedegebuur@gmail.com>
2024-05-16 14:04:37 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+pub use serialize_internal::{ SBI, Serialize, DeSerialize };
+pub use serialize_macro::{ Serialize, DeSerialize };
+
+#[cfg(test)]
+mod tests {
+
+    use crate::{ Serialize, DeSerialize, SBI };
+
+    #[derive(Serialize, DeSerialize, Debug)]
+    struct TStruct {
+        name: String,
+        num: f32,
+    }
+
+    #[test]
+    fn it_works() {
+        let tstruct = TStruct { name: "peetah".to_string(), num: 5634.98 };
+        let mut sbi = SBI::new();
+        tstruct.serialize(&mut sbi);
+
+        let tstruct = sbi.deserialize::<TStruct>();
+        println!("{:#?}", tstruct);
+    }
+}
